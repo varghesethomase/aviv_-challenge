08487e18aad62652fb219d00cf8c74cf3cf929ec
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 00:26:35 +0200

    renaming filter hook

--- src/hooks/queries/filters.ts
+++ src/hooks/queries/filters.ts
@@ -2,22 +2,27 @@ import {createQuery} from "react-query-kit"
 import {API_BASE_URL} from "../../configs"
 
 type Response = {isAuthenticated: boolean}
-type Variables = {username: string; password: string}
+type Variables = {scope: string}
+export enum FilterTypes {
+  Input = "input",
+  Select = "select",
+}
 
-const useAuthenticate = createQuery<Response, Variables, Error>({
-  primaryKey: `${API_BASE_URL}/login`,
-  queryFn: ({
-    queryKey: [primaryKey, variables],
-  }: {
-    queryKey: [string, Variables]
-  }) => {
-    console.log("here", primaryKey)
+export enum InputTypes {
+  Button = "button",
+  Text = "text",
+  CheckBox = "checkbox",
+  Email = "email",
+}
+
+const useFetchFilters = createQuery<Response, Variables, Error>({
+  primaryKey: `${API_BASE_URL}/filters`,
+  queryFn: ({queryKey: [primaryKey]}: {queryKey: [string, Variables]}) => {
     return fetch(primaryKey, {
-      method: "POST",
-      body: JSON.stringify(variables),
+      method: "GET",
     }).then((res) => res.json())
   },
-  suspense: false,
+  suspense: true,
 })
 
-export {useAuthenticate}
+export {useFetchFilters}
