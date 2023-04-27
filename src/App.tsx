49b272690ce5c9241652d9ad28fa6f1159a949a5
Vakar Eeralil <varghese@m5nv.com>
2023-04-27 11:32:38 +0200

    moving provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Dialog from "./components/Dialog/Dialog"
 import "./App.css"
 
 // Start the mock service worker. Can be removed when replaced with the real server
 import browser from "./mocks/browser"
+import {useAuthenticate} from "./hooks/queries/filters"
 if (import.meta.env.DEV) {
   browser.start()
 }
 
-const queryClient = new QueryClient()
+const variables = {username: "varghese", password: "test123"}
 
 function App() {
+  const {data} = useAuthenticate({variables, suspense: true})
+  console.log(data)
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <button>select filters</button>
       <Dialog />
-    </QueryClientProvider>
+    </>
   )
 }
 
