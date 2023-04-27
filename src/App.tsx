00cdaf98ea83bf0a96e0393b91fccc876a6bf21b
Vakar Eeralil <varghese@m5nv.com>
2023-04-27 23:15:55 +0200

    basic modal with esc and outside click support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Dialog from "./components/Dialog/Dialog"
+import "normalize.css"
+import "@fontsource/roboto"
 import "./App.css"
 
 // Start the mock service worker. Can be removed when replaced with the real server
 import browser from "./mocks/browser"
 import {useAuthenticate} from "./hooks/queries/filters"
+import {useState} from "react"
 if (import.meta.env.DEV) {
   browser.start()
 }
@@ -11,12 +14,29 @@ if (import.meta.env.DEV) {
 const variables = {username: "varghese", password: "test123"}
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const {data} = useAuthenticate({variables, suspense: true})
-  console.log(data)
+
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true)
+  }
+
   return (
     <>
-      <button>select filters</button>
-      <Dialog />
+      <button onClick={handleModalOpen}>select filters</button>
+      <Dialog
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        classes="filter-dialog"
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick={true}
+      >
+        <div>hello world</div>
+      </Dialog>
     </>
   )
 }
