34253947555131cd96d9fc25b14dd4511ab7f915
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 11:29:33 +0200

    cleanup

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,6 @@ import {rest} from "msw"
 import {API_BASE_URL} from "../configs"
 export const handlers = [
   rest.get(`${API_BASE_URL}/filters`, (_req, res, ctx) => {
-    console.log("in the server")
     return res(
       ctx.json({
         name: "",
