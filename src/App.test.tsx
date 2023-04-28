2b59e72b2dd804e54e51b01a24441ba8e3dacd97
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 16:28:59 +0200

    tests for functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import {screen, render} from "@testing-library/react"
+import {screen, render, fireEvent, waitFor} from "@testing-library/react"
 import {describe, expect, it} from "vitest"
 import App from "./App"
 
@@ -13,5 +13,18 @@ describe("Describe", () => {
       ).toBeInTheDocument()
     })
   })
-  // describe("Functionality", () => {})
+  describe("Functionality", () => {
+    it("opens the modal on clicking the select filters button", async () => {
+      expect(screen.getByTestId("dialog")).not.toHaveAttribute("open")
+
+      fireEvent.click(
+        screen.getByRole("button", {
+          name: /select filters/i,
+        })
+      )
+      await waitFor(() =>
+        expect(screen.getByTestId("dialog")).toHaveAttribute("open")
+      )
+    })
+  })
 })
