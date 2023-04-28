ded12a24d5627a41533994728c8a077f10c7232f
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 14:42:11 +0200

    using mock response and fixing tests

--- src/hooks/queries/filters.test.tsx
+++ src/hooks/queries/filters.test.tsx
@@ -2,13 +2,14 @@ import {renderHook, waitFor} from "@testing-library/react"
 import {useFetchFilters} from "./filters"
 import {describe, expect, it} from "vitest"
 import {createQueryProviderWrapper} from "../../utils/testing/create-query-provider-wrapper"
+import {getFiltersResponse} from "../../mocks/data"
 
 describe("useFetchFilters", () => {
   it("should return filters", async () => {
-    const {result} = renderHook(() => useFetchFilters({variables: null}), {
+    const {result} = renderHook(() => useFetchFilters(), {
       wrapper: createQueryProviderWrapper(),
     })
     await waitFor(() => expect(result.current.isSuccess).toBe(true))
-    await waitFor(() => expect(result.current.data).toBe(true))
+    await waitFor(() => expect(result.current.data).toEqual(getFiltersResponse))
   })
 })
