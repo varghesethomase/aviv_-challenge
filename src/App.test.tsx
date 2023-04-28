45853a504b39ded2d947598cc7e75a7394440db7
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 17:21:13 +0200

    tests for showing and hiding of dialog

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,5 @@
+// @vitest-environment happy-dom
+
 import {screen, render, fireEvent, waitFor} from "@testing-library/react"
 import {describe, expect, it} from "vitest"
 import App from "./App"
@@ -15,6 +17,8 @@ describe("Describe", () => {
   })
   describe("Functionality", () => {
     it("opens the modal on clicking the select filters button", async () => {
+      render(<App />)
+
       expect(screen.getByTestId("dialog")).not.toHaveAttribute("open")
 
       fireEvent.click(
@@ -24,6 +28,42 @@ describe("Describe", () => {
       )
       await waitFor(() =>
         expect(screen.getByTestId("dialog")).toHaveAttribute("open")
+      )
+    })
+    it("closes the modal on clicking the close modal button", async () => {
+      render(<App />)
+
+      expect(screen.getByTestId("dialog")).not.toHaveAttribute("open")
+      fireEvent.click(
+        screen.getByRole("button", {
+          name: /select filters/i,
+        })
+      )
+      await waitFor(() => {
+        expect(screen.getByTestId("dialog")).toHaveAttribute("open")
+        expect(screen.getByTestId("dialog-close")).toBeInTheDocument()
+      })
+      fireEvent.click(screen.getByTestId("dialog-close"), {focus: true})
+      await waitFor(() =>
+        expect(screen.getByTestId("dialog")).toHaveClass("dialog--is-hidden")
+      )
+    })
+
+    it("adds the class dialog--open to the body when dialog is open and removes it when closed", async () => {
+      render(<App />)
+      expect(screen.getByTestId("dialog")).not.toHaveAttribute("open")
+      fireEvent.click(
+        screen.getByRole("button", {
+          name: /select filters/i,
+        })
+      )
+      await waitFor(() => {
+        expect(screen.getByTestId("dialog")).toHaveAttribute("open")
+      })
+      expect(document.body?.classList.contains("dialog--open")).toBeTruthy()
+      fireEvent.click(screen.getByTestId("dialog-close"))
+      await waitFor(() =>
+        expect(document.body?.classList.contains("dialog--open")).toBeFalsy()
       )
     })
   })
