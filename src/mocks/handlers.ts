fc20ca4c1f546e978d91ca994a9de6757e376033
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 14:43:06 +0200

    response mock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,16 +1,9 @@
 // src/mocks/handlers.js
 import {rest} from "msw"
 import {API_BASE_URL} from "../configs"
+import {getFiltersResponse} from "./data"
 export const handlers = [
   rest.get(`${API_BASE_URL}/filters`, (_req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        name: "abc",
-        description: "",
-        value: "",
-        type: "",
-      })
-    )
+    return res(ctx.status(200), ctx.json(getFiltersResponse))
   }),
 ]
