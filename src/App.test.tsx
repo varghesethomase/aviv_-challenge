f42e90f55bb7f0cc29526afb25df72c66aa11edd
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 15:08:00 +0200

    checking if show filters button is available

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
+import {screen, render} from "@testing-library/react"
 import {describe, expect, it} from "vitest"
+import App from "./App"
 
 describe("Describe", () => {
   describe("Rendering", () => {
-    it("displays the modal ")
+    it("displays the select filters button", () => {
+      render(<App />)
+      expect(
+        screen.getByRole("button", {
+          name: /select filters/i,
+        })
+      ).toBeInTheDocument()
+    })
   })
-  describe("Functionality", () => {})
+  // describe("Functionality", () => {})
 })
