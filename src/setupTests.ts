54d1a224fac0aa319d693f515840648374542947
Vakar Eeralil <varghese@m5nv.com>
2023-04-28 15:14:52 +0200

    extending jest matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,10 @@
 import {afterAll, afterEach, beforeAll} from "vitest"
-import server from "./mocks/server"
+import matchers from "@testing-library/jest-dom/matchers"
+import {expect} from "vitest"
 import {fetch, Headers, Request, Response} from "cross-fetch"
+import server from "./mocks/server"
+
+expect.extend(matchers)
 
 global.fetch = fetch
 global.Headers = Headers
